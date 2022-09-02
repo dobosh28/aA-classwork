d8e8a164fbbfad00553fc83ad18c741b1e85ef3b
Yosyp Dobosh <joseph.dobosh@gmail.com>
2022-09-02 11:06:21 -0400

    create sessions controller and view

---
+++ W7/W7D5/Reddit_Clone/app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+    before_action :require_logged_in, only: [:create, :destroy]
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+
+        if @user.nil?
+            @user = User.new(username: params[:user][:username])
+            flash.now[:errors] = ['Invalid username or password']
+            render :new
+        else
+            login(@user)
+            redirect_to users_url
+        end
+    end
+
+    def destroy
+        logout!
+        redirect_to new_session_url
+    end
+end
